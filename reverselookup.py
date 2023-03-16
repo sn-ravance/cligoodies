9b73bc5084023690d2773ee9d5f7ad13ace65b5e
sealmindset <sealmindset@gmail.com>
2023-03-16 11:20:01 -0500

    Update reverselookup.py

--- reverselookup.py
+++ reverselookup.py
@@ -1,17 +1,9 @@
 #
-# Description
-#
-# Parse a CSV file with IP addresses or FQDN and do a reverse lookup
-#
-## Purpose
-#
-# A Splunk helper file
-#
 ## Instructions
 #
 # 1. Run the following query in Splunk to discover all of the internal assets
 #
-# index="name of index" source="name of source" 
+# index="name of index" sourcetype="name of source" 
 # | where dns_request_queried_domain like "%.<target domain name>.com" 
 # | dedup dns_request_queried_domain
 # | rex field=dns_request_queried_domain "^(?<fqdn>[^ ]*)" 
@@ -20,15 +12,12 @@
 #
 # 2. Export the results (limit to less than 10,000 - per Splunk) as a csv file
 #
+#    The CSV should look like this -
+#
 #     "internal_ip","fqdn"
 #
 # 3. Run this script
-#    
-#     python3 lookup.py --nmae <name of csv file>.csv
-#
-# 4. Output of the new file is <name of csv file>_intip2extip.csv
-#
-#     "internal_ip","fqdn","external_ip"
+# 4. Review the results
 #
 import socket
 import argparse
@@ -38,14 +27,35 @@ from colorama import init as colorama_init
 from colorama import Fore
 from colorama import Style
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--name', type=str, required=True, help='source csv file')
-parser.add_argument('--column', type=int, required=True, help='column number starting from zero')
-parser.add_argument('--lookup', type=str, required=True, help='by domain | ip')
+arg_desc = '''\
+Author: sealmindset
+Version: 1.0
+Date: 02/16/2023
+
+Description:
+Identify DNS names (or IP Address) that are resolvable externally.
+
+Potential Use Case:
+A list of internal DNS names and IP Addresses.
+
+Prerequisite:
+A CSV file containing a list of IP Addresses and/or FQDN.
+The column number of the attribute to do a reverse lookup on.
+        '''
+parser = argparse.ArgumentParser(formatter_class = argparse.RawDescriptionHelpFormatter,
+                                    description= arg_desc)
+
+parser.add_argument('-n','--name', type=str, required=True, help='source csv file')
+parser.add_argument('-c','--column', type=int, required=True, help='column number starting from zero')
+parser.add_argument('-l','--lookup', type=str, required=True, help='by domain | ip')
+parser.add_argument('-o','--output', type=str, required=True, help='append to the filename')
+
 args = parser.parse_args()
 
 infile = args.name
-outfile = infile.replace(".csv", "_intip2extip.csv")
+ofileext = args.output
+
+outfile = infile.replace(".csv", "_" + ofileext + ".csv")
 
 colnum = args.column
 
@@ -69,8 +79,7 @@ def ip_address(fqdn):
     print(f"{status.FAIL} Could not resolve " + fqdn + " for", ip_addr)
   return ip_addr
 
-with open(infile, 'r') as read_obj, \
-        open(outfile, 'w', newline='') as write_obj:
+with open(infile, 'r') as read_obj, open(outfile, 'w', newline='') as write_obj:
     csv_reader = reader(read_obj)
     csv_writer = writer(write_obj)
     for row in csv_reader:
