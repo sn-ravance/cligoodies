1053f99880ca40bcd5bcd6d508ebdf83189e3365
sn-ravance <88193495+sn-ravance@users.noreply.github.com>
2023-04-12 08:05:43 -0500

    Create csvfile.sh

---
+++ csvfile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 
+# Description: Feed a csv file to a python script as arguments
+
+while IFS=', ' read -r SUBNAME SUBID
+do
+    #You can do your processing here, e.g.
+    echo "$SUBID $SUBNAME"
+    python3 azurecliscript.py -c -s $SUBID -o $SUBNAME.json
+done < azsubscription.txt
+
+exit 0
