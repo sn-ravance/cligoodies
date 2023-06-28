61e62464076e596790dce5aaae3a73860e7a82b8
sn-ravance <88193495+sn-ravance@users.noreply.github.com>
2023-06-27 22:04:07 -0500

    Update profile.sh

--- profile.sh
+++ profile.sh
@@ -11,6 +11,6 @@ while IFS=',' read -r _ value _; do
   echo "Processing value: $value"
 
   urlpf="https://api.atlassian.com/users/$value/manage/profile"
-  curl --request GET --url $urlpf --header 'Authorization: Bearer ATCTT3xFfGN0psvWPL7VgEr6iYqSDah_Bbza6rDR6voiSGpKB8YryTQP9iaXzeDVY8yBI_gIZUZ1OcP-pPXsj4Ddkzk0XqKVY2h0OiXKsM93qc65lTu1zUwxCkEcpnZZvMes72o2yqI_Trhyz7QuuRcu7FeIsgEwJUN8-eDP2H1ndj6BcqCfoJQ=6684BFA4' --header 'Accept: application/json' | jq .
+  curl --request GET --url $urlpf --header 'Authorization: Bearer KEY' --header 'Accept: application/json' | jq .
 
 done < "$csv_file"
